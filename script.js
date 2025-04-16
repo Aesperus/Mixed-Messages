7ec4a491ceba475d40a2c68a6bfb19fa2b98fadd
Aesperus <email@email.com>
2025-04-16 11:32:32 +0300

    Create getMessage method and random number generator function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const messageFill = {
-    day: ["horrible", "very bad", "bad", "not great", "normal", "not bad", "good", "great", "terrific"],
-    area: ["health", "love life", "work", "responisibilities", "friends", "finances", "relatives"]
+function genRandom(number) {
+    return Math.floor(Math.random() * number);
 }
 
+const messageFill = {
+    _day: ["horrible", "very bad", "bad", "not great", "normal", "not bad", "good", "great", "terrific"],
+    _area: ["health", "love life", "work", "responisibilities", "friends", "finances", "relatives"],
+    getMessage () {
+        const randomDay = genRandom(this._day.length);
+        const randomLuckArea = genRandom(this._area.length);
+        let randomConcernArea;
+        do {
+        randomConcernArea = genRandom(this._area.length);
+        } while (randomConcernArea === randomLuckArea);
+        console.log(`Today is a ${this._day[randomDay]} day.\nYou will have good fortune with your ${this._area[randomLuckArea]}.\nBe careful with and pay special attention to your ${this._area[randomConcernArea]}.`);
+    }
+}
