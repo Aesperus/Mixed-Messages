f6dcf1e78a3ab33e3cde2954f27e687f1162c088
Aesperus <email@email.com>
2025-04-16 11:17:49 +0300

    Create random message container

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello");
+const messageFill = {
+    day: ["horrible", "very bad", "bad", "not great", "normal", "not bad", "good", "great", "terrific"],
+    area: ["health", "love life", "work", "responisibilities", "friends", "finances", "relatives"]
+}
+
